9827dcef39e765f3afa89644dc34b6db192624bf
Darko <darkogligorijevic4@gmail.com>
2023-05-08 11:19:01 +0200

    change reviews routes, and add validation on reviews routes

--- Aplikacija/server/controllers/reviews-controllers.js
+++ Aplikacija/server/controllers/reviews-controllers.js
@@ -1,3 +1,5 @@
+const {validationResult} = require('express-validator')
+
 const HttpError = require('../models/HttpError')
 const Post = require('../models/Post');
 const Review = require('../models/Reviews')
@@ -5,8 +7,15 @@ const Review = require('../models/Reviews')
 //////////////////////////////////////////////////////////////////////////////////////////
 
 module.exports.postReviewByPostId = async (req,res,next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError('Invalid inputs passed, please check your data.', 422)
+    );
+  }
   const {postId} = req.params
   const {star, reviewText,userId} = req.body
+  console.log(typeof star)
   let post;
   try {
     post = await Post.findOne({_id:postId})
@@ -20,6 +29,19 @@ module.exports.postReviewByPostId = async (req,res,next) => {
     return next(error)
   }
 
+  let review;
+  try {
+    review = await Review.findOne({userId:userId,postId:postId})
+  } catch(err) {
+    const error = new HttpError('Something went wrong',500)
+    return next(error)
+  }
+
+  if(review) {
+    const error = new HttpError('You have already create a review for such a post',409)
+    return next(error)
+  }
+
   const newReview = new Review({
     postId, //postId:postId
     userId, // kasnije u cu u req da dobijem userId ovo je samo testa radi
@@ -27,17 +49,75 @@ module.exports.postReviewByPostId = async (req,res,next) => {
     reviewText
   })
 
-  console.log(newReview)
-
   try {
     await newReview.save()
+  } catch(err) {
+    const error = new HttpError('Something went wrong',500)
+    return next(error)
+  }
+
+  res.status(201).json({message:"Review has been added"})
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+module.exports.patchReviewByPostId = async (req,res,next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError('Invalid inputs passed, please check your data.', 422)
+    );
+  }
+  const {postId,reviewId} = req.params
+  const {star, reviewText,userId} = req.body
+  let post;
+  try {
+    post = await Post.findOne({_id:postId})
+  } catch(err) {
+    console.log('1')
+    const error = new HttpError('Something went wrong',500)
+    return next(error)
+  }
+
+  if (!post) {
+    const error = new HttpError('There is no such a post',404)
+    return next(error)
+  }
+
+  let review;
+  try {
+    review = await Review.findOne({_id:reviewId})
   } catch(err) {
     console.log('2')
     const error = new HttpError('Something went wrong',500)
     return next(error)
   }
 
-  res.status(201).json({message:"Review has been added"})
+
+
+  if (!review || review.userId.toString() !== userId || review.postId.toString() !== postId) {
+    const error = new HttpError('There is no such a review for this post, or you are not allowed to update this review',404)
+    return next(error)
+  }
+
+  const updateReview = new Review({
+    _id:review._id,
+    postId, //postId:postId
+    userId, // kasnije u cu u req da dobijem userId ovo je samo testa radi
+    star: +star || review.star,
+    reviewText: reviewText || review.reviewText
+  })
+
+
+  try {
+    await Review.findOneAndUpdate({_id:reviewId},updateReview);
+  } catch(err) {
+    const error = new HttpError('Something went wrong',500)
+    return next(error)
+  }
+
+  res.status(200).json({message:"Review has been updated"})
+
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -71,3 +151,37 @@ module.exports.getReviewsByPostId = async (req,res,next) => {
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
+
+module.exports.deleteReviewByPostId = async (req,res,next) => {
+  const {reviewId,userId} = req.params;
+  console.log(reviewId)
+  console.log(userId)
+  let review;
+  try {
+    review = await Review.findOne({_id:reviewId})
+  } catch(err) {
+    const error = new HttpError('Something went wrong',500)
+    return next(error)
+  }
+
+  console.log(review)
+
+
+  if (!review || review.userId.toString() !== userId ) {
+    console.log('jel doso ovde')
+    const error = new HttpError('There is no such a review , or you are not allowed to delete this review',404)
+    return next(error)
+  }
+
+  try {
+    await Review.findOneAndDelete({_id:reviewId})
+  } catch(err) {
+    const error = new HttpError('There is no such a post',404)
+    return next(error)
+  }
+
+  res.status(200).json({message:"Review has been deleted"})
+
+
+}
+
--- Aplikacija/server/routes/reviews-routes.js
+++ Aplikacija/server/routes/reviews-routes.js
@@ -1,10 +1,25 @@
 const router = require('express').Router()
+const {check} = require('express-validator')
 
 const reviewControllers = require('../controllers/reviews-controllers')
 
-router.post('/:postId',reviewControllers.postReviewByPostId)
+router.post('/postId/:postId',
+[
+  check('star').isFloat({min:1, max:5}),
+  check('reviewText').trim().not().isEmpty().isLength({min:10})
+],
+reviewControllers.postReviewByPostId)
 
-router.get('/:postId',reviewControllers.getReviewsByPostId)
+router.patch('/id/:reviewId/post/:postId',
+[
+  check('star').isFloat({min:1, max:5}),
+  check('reviewText').trim().not().isEmpty().isLength({min:10})
+]
+,reviewControllers.patchReviewByPostId)
+
+router.get('/all/:postId',reviewControllers.getReviewsByPostId);
+
+router.delete('/:reviewId/testUserId/:userId',reviewControllers.deleteReviewByPostId); // userId se nece slati preko dynamic path parametara
 
 
 module.exports = router;
